9fe1cda7a068a53cadb652a910853cf1abf347ab
MenesiZoltan <colos.menesi@gmail.com>
2019-09-02 11:30:10 +0200

    Created separate class for Greeter

---
+++ ConsoleApplication1/ConsoleApplication1/Greeter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public static class Greeter
+    {
+        public static void Greeting()
+        {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("This is the second Commit! Next time we will create a separate class!");
+            Console.ReadLine();
+        }
+    }
+}
--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,9 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
-            Console.WriteLine("This is the second Commit! Next time we will create a separate class!");
-            Console.ReadLine();
+            Greeter.Greeting();
         }
     }
 }
