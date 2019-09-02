6b4f31ef9a1dd458e9acd14c248910ef684879ce
MenesiZoltan <colos.menesi@gmail.com>
2019-09-02 11:57:37 +0200

    Created Speaker Class

---
+++ ConsoleApplication1/ConsoleApplication1/Speaker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public static class Speaker
+    {
+        public static void SpeakStuff()
+        {
+            Console.WriteLine("I am Lorax and i speak for the trees.");
+            Console.ReadLine();
+        }
+    }
+}
